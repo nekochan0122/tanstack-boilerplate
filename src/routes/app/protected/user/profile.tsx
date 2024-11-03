6ad8a016ab3286f9b83b241050740fda34da4a4a
NekoChan <aaron74327@gmail.com>
2024-11-03 05:27:12 -0800

    Update profile.tsx

--- src/routes/app/protected/user/profile.tsx
+++ src/routes/app/protected/user/profile.tsx
@@ -1,9 +1,29 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar'
+import { useAuthedQuery } from '~/services/auth.query'
+
 export const Route = createFileRoute('/_protected-user/user/profile')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return 'Hello /_protected-user/user/profile!'
+  const authedQuery = useAuthedQuery()
+
+  return (
+    <div className='flex flex-col items-center space-y-4'>
+      <Avatar className='size-32 rounded-full'>
+        <AvatarImage src={authedQuery.data.user.image || undefined} alt={authedQuery.data.user.name} />
+        <AvatarFallback className='rounded-lg text-5xl'>{authedQuery.data.user.name[0]}</AvatarFallback>
+      </Avatar>
+      <div className='space-y-2 text-center'>
+        <h2 className='text-2xl'>
+          {authedQuery.data.user.name}
+        </h2>
+        <p>
+          Joined {new Date(authedQuery.data.user.createdAt).toDateString()}
+        </p>
+      </div>
+    </div>
+  )
 }
