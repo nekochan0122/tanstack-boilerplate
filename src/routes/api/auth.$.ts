de616895c4707161acdc2bc12fecdb591cdfb361
NekoChan <aaron74327@gmail.com>
2024-11-03 04:25:11 -0800

    auth handler only allow oauth sign-in

--- src/routes/api/auth.$.ts
+++ src/routes/api/auth.$.ts
@@ -1,8 +1,32 @@
 import { createAPIFileRoute } from '@tanstack/start/api'
+import type { StartAPIMethodCallback } from '@tanstack/start/api'
 
 import { auth } from '~/libs/auth'
 
 export const Route = createAPIFileRoute('/api/auth/$')({
-  GET: ({ request }) => auth.handler(request),
-  POST: ({ request }) => auth.handler(request),
+  GET: createAuthHandler(),
+  POST: createAuthHandler(),
 })
+
+const allowedPaths = [
+  /^\/api\/auth\/sign-in\/social$/,
+  /^\/api\/auth\/callback\/.+$/,
+]
+
+function createAuthHandler() {
+  const authHandler: StartAPIMethodCallback<'/api/auth/$'> = ({ request }) => {
+    const path = new URL(request.url).pathname
+
+    const isAllowed = allowedPaths.some((regExp) => regExp.test(path))
+
+    if (!isAllowed) {
+      return new Response('Not allowed', {
+        status: 403,
+      })
+    }
+
+    return auth.handler(request)
+  }
+
+  return authHandler
+}
