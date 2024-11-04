6dec4d8848f1dd13bd24f8bef0056133ddcd30cd
NekoChan <aaron74327@gmail.com>
2024-11-04 11:52:52 -0800

    create shadcn preset

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,75 +1,19 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
-import tailwindAnimatePlugin from 'tailwindcss-plugin-animate'
 import type { Config } from 'tailwindcss'
 
+import { shadcnPreset } from './plugins/tailwind/shadcn-preset'
+
 export default {
-  darkMode: ['class', '[data-theme="dark"]'],
-  content: ['./src/**/*.tsx'],
+  presets: [shadcnPreset({ color: 'zinc' })],
+  content: [
+    './src/components/**/*.tsx',
+    './src/routes/**/*.tsx',
+  ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter Variable', 'Noto Sans TC Variable', ...defaultTheme.fontFamily.sans],
       },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
-      colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        chart: {
-          1: 'hsl(var(--chart-1))',
-          2: 'hsl(var(--chart-2))',
-          3: 'hsl(var(--chart-3))',
-          4: 'hsl(var(--chart-4))',
-          5: 'hsl(var(--chart-5))',
-        },
-        sidebar: {
-          'DEFAULT': 'hsl(var(--sidebar-background))',
-          'foreground': 'hsl(var(--sidebar-foreground))',
-          'primary': 'hsl(var(--sidebar-primary))',
-          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-          'accent': 'hsl(var(--sidebar-accent))',
-          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-          'border': 'hsl(var(--sidebar-border))',
-          'ring': 'hsl(var(--sidebar-ring))',
-        },
-      },
     },
   },
-  plugins: [
-    tailwindAnimatePlugin,
-  ],
 } satisfies Config
