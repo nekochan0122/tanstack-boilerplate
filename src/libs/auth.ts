0fc00b06da5ebeaaaad70ca4848bbae267b78b89
NekoChan <aaron74327@gmail.com>
2024-11-05 04:13:31 -0800

    Update auth.ts

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -10,6 +10,7 @@ import { PASSWORD_MAX, PASSWORD_MIN } from '~/services/auth.schema'
 
 export type Auth = z.infer<typeof authSchema>
 export type Authed = Extract<Auth, { isAuthenticated: true }>
+
 export type AuthAPI = keyof typeof auth.api
 export type InferAuthOptions<API extends AuthAPI> = SimplifyDeep<NonNullable<Parameters<typeof auth.api[API]>[0]>>
 export type InferAuthResponse<API extends AuthAPI> = SimplifyDeep<Awaited<ReturnType<typeof auth.api[API]>>>
