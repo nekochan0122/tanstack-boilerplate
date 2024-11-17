802e248893c281df1fe24442a94a88df77b346ee
NekoChan <aaron74327@gmail.com>
2024-11-16 17:19:17 -0800

    Update session.ts

--- src/libs/session.ts
+++ src/libs/session.ts
@@ -7,8 +7,8 @@ import type { I18nSession } from '~/libs/i18n'
 
 type VinxiSession = Simplify<Partial<I18nSession>>
 
-export const getVinxiSessionHelper = async () => {
-  return await useSession<VinxiSession>({
+export const getVinxiSessionHelper = () => {
+  return useSession<VinxiSession>({
     name: 'vinxi-session',
     password: process.env.APP_SECRET,
   })
