e7401f5e23f2408026572e53da05dc561f25a0c4
NekoChan <aaron74327@gmail.com>
2024-11-18 15:17:46 -0800

    remove debug code

--- src/routes/api/hello.ts
+++ src/routes/api/hello.ts
@@ -1,17 +1,7 @@
 import { createAPIFileRoute } from '@tanstack/start/api'
 
-import { VerificationEmail } from '~/emails/verification-email'
-import { sendEmail } from '~/libs/email'
-
 export const Route = createAPIFileRoute('/api/hello')({
   GET: async ({ request }) => {
-    await sendEmail({
-      from: 'John Doe <TanStack@example.com>',
-      to: 'aaron74327@gmail.com',
-      subject: 'Hello, World!',
-      react: VerificationEmail({ url: 'https://google.com' }),
-    })
-
     return new Response('Hello, World! from ' + request.url)
   },
 })
