fd8e505bc8a9a54df69e9ee4bd66a128e1296072
NekoChan <aaron74327@gmail.com>
2024-11-18 15:18:02 -0800

    Delete hello.ts

--- src/routes/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import { createAPIFileRoute } from '@tanstack/start/api'
-
-export const Route = createAPIFileRoute('/api/hello')({
-  GET: async ({ request }) => {
-    return new Response('Hello, World! from ' + request.url)
-  },
-})
