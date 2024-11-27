d5418d20e023ad260d6501d3f35167dfb0732028
NekoChan <aaron74327@gmail.com>
2024-11-27 11:11:00 -0800

    update deps

--- src/routes/api/auth.$.ts
+++ src/routes/api/auth.$.ts
@@ -2,7 +2,7 @@ import { createAPIFileRoute } from '@tanstack/start/api'
 
 import { auth } from '~/server/auth'
 
-export const Route = createAPIFileRoute('/api/auth/$')({
+export const APIRoute = createAPIFileRoute('/api/auth/$')({
   GET: ({ request }) => auth.handler(request),
   POST: ({ request }) => auth.handler(request),
 })
