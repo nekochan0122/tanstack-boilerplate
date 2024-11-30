324029818c82f8233af212712890debf09610852
NekoChan <aaron74327@gmail.com>
2024-11-30 00:05:06 -0800

    remove react-scan

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -3,7 +3,6 @@
 import { StartClient } from '@tanstack/start'
 import { StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
-import { scan } from 'react-scan'
 
 import { createRouter } from '~/router'
 
@@ -11,11 +10,6 @@ const router = createRouter()
 
 window.getRouter = () => router
 window.getQueryClient = () => router.options.context.queryClient
-
-scan({
-  enabled: import.meta.dev,
-  log: true,
-})
 
 hydrateRoot(document, (
   <StrictMode>
