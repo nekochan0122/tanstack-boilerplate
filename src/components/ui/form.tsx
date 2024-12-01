629adad4d2289e91e82cfc60f7a4d207928453a4
NekoChan <aaron74327@gmail.com>
2024-12-01 14:47:29 -0800

    add useForm return type `FormExtended`

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -4,7 +4,7 @@
 
 import { useField as useFieldApi, useForm as useTanStackForm } from '@tanstack/react-form'
 import { zodValidator } from '@tanstack/react-form-zod-adapter'
-import type { DeepKeys, DeepValue, FieldApi, FormOptions, Validator } from '@tanstack/react-form'
+import type { DeepKeys, DeepValue, FieldApi, FormOptions, ReactFormExtendedApi, Validator } from '@tanstack/react-form'
 import type { UseFieldOptions } from 'node_modules/@tanstack/react-form/dist/esm/types'
 import type { ChangeEvent, ComponentProps, FC, ReactNode } from 'react'
 import type { Except } from 'type-fest'
@@ -30,8 +30,8 @@ interface FieldControllerProps extends ComponentProps<typeof Slot> {}
 interface FieldApiExtended<
   TParentData,
   TName extends DeepKeys<TParentData>,
-  TFieldValidator extends Validator<DeepValue<TParentData, TName>, unknown> | undefined = undefined,
-  TFormValidator extends Validator<TParentData, unknown> | undefined = undefined,
+  TFieldValidator extends Validator<DeepValue<TParentData, TName>, unknown>,
+  TFormValidator extends Validator<TParentData, unknown>,
   TData extends DeepValue<TParentData, TName> = DeepValue<TParentData, TName>,
 > extends FieldApi<TParentData, TName, TFieldValidator, TFormValidator, TData> {
   Label: FC<FieldLabelProps>
@@ -45,8 +45,8 @@ interface FieldApiExtended<
 interface FieldComponentProps<
   TParentData,
   TName extends DeepKeys<TParentData>,
-  TFieldValidator extends Validator<DeepValue<TParentData, TName>, unknown> | undefined = undefined,
-  TFormValidator extends Validator<TParentData, unknown> | undefined = undefined,
+  TFieldValidator extends Validator<DeepValue<TParentData, TName>, unknown>,
+  TFormValidator extends Validator<TParentData, unknown>,
   TData extends DeepValue<TParentData, TName> = DeepValue<TParentData, TName>,
 > extends UseFieldOptions<TParentData, TName, TFieldValidator, TFormValidator, TData> {
   render: (fieldApi: FieldApiExtended<TParentData, TName, TFieldValidator, TFormValidator, TData>) => ReactNode
@@ -54,14 +54,22 @@ interface FieldComponentProps<
 
 type FieldComponent<
   TParentData,
-  TFormValidator extends Validator<TParentData, unknown> | undefined = undefined,
+  TFormValidator extends Validator<TParentData, unknown>,
 > = <
   TName extends DeepKeys<TParentData>,
-  TFieldValidator extends Validator<DeepValue<TParentData, TName>, unknown> | undefined = undefined,
+  TFieldValidator extends Validator<DeepValue<TParentData, TName>, unknown>,
   TData extends DeepValue<TParentData, TName> = DeepValue<TParentData, TName>,
 >({ render, ...fieldOptions }: Except<FieldComponentProps<TParentData, TName, TFieldValidator, TFormValidator, TData>, 'form'>) => ReactNode
 
 type AnyFieldApi = FieldApi<any, any, any, any, any>
+
+type FormExtended<
+  TFormData,
+> = ReactFormExtendedApi<TFormData, Validator<TFormData>> & {
+  Root: FC<ComponentProps<'form'>>
+  Field: FieldComponent<TFormData, Validator<TFormData>>
+  Submit: FC<ComponentProps<typeof Button>>
+}
 
 const [FieldContextProvider, useFieldContext] = createContextFactory<AnyFieldApi>()
 
@@ -71,7 +79,7 @@ function useForm<
 >(
   schema: TFormSchema,
   options?: Except<FormOptions<TFormData, Validator<TFormData>>, 'validatorAdapter'>,
-) {
+): FormExtended<TFormData> {
   const form = useTanStackForm({
     validatorAdapter: zodValidator({
       transformErrors: (errors) => errors.map((e) => e.message)[0],
@@ -130,7 +138,7 @@ function useForm<
     Root: FormRoot,
     Field: FormField,
     Submit: FormSubmit,
-  }
+  } as FormExtended<TFormData>
 }
 
 function useField() {
