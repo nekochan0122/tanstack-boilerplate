847e9f937ca13f896fe093f3b01d7dbd6e0f7b32
NekoChan <aaron74327@gmail.com>
2024-12-01 15:07:51 -0800

    create ExcludeUnionStrict

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -12,6 +12,7 @@ export type StringNumber = `${number}`
 export type StringBoolean = `${boolean}`
 
 export type ExtractUnionStrict<T, U extends T> = Extract<T, U>
+export type ExcludeUnionStrict<T, U extends T> = Exclude<T, U>
 
 export function cx(...inputs: ClassArray) {
   return twMerge(clsx(inputs))
